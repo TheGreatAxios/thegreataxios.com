eb4a047a6e6b338dcbaa5bdf85424d968e1657bf
TheGreatAxios <sawyer@dirtroad.dev>
2025-10-20 17:05:25 -0700

    Update Match

--- vocs.config.ts
+++ vocs.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vocs'
 
 export default defineConfig({
   title: 'thegreataxios blog',
+  baseUrl: "https://thegreataxios.com",
+  description: "The personal blog of TheGreatAxios",
+  iconUrl: {
+    light: '/thegreataxios.png',
+    dark: '/thegreataxios.png',
+  },
   socials: [
     {
       icon: "github",
@@ -14,16 +20,19 @@ export default defineConfig({
   ],
   topNav: [
     {
+      link: "/",
+      text: "Blog",
+      match: ""
+    },
+    {
       link: "/my-projects",
-      text: "My Projects"
+      text: "My Projects",
+      match: "/my-projects"
     },
     {
       link: "/sponsor",
-      text: "Sponsor"
-    },
-    {
-      link: "/blog",
-      text: "Blog"
+      text: "Sponsor",
+      match: "/sponsor"
     },
     {
       link: "https://buymeacoffee.com/thegreataxios",
