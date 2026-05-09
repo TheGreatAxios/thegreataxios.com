7f5eea78e1ed2f0312581ee184480687b9ecc597
thegreataxios <20197130+TheGreatAxios@users.noreply.github.com>
2026-05-08 21:21:22 -0700

    Fix Broken Link

--- docs/snippets/Hero.tsx
+++ docs/snippets/Hero.tsx
@@ -10,7 +10,7 @@ interface HeroProps {
 export default function Hero({
   name = "Sawyer Cutler",
   github = "https://github.com/thegreataxios",
-  x = "https://linkedin.com/in/sawyercutler",
+  x = "https://x.com/thegreataxios",
   linkedin = "https://linkedin.com/in/sawyercutler"
 }: HeroProps) {
   return (
